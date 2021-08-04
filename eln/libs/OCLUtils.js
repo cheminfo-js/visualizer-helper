6f7800b94c9436cf524a48d29c706f9ec0d090d9
Luc Patiny <luc@patiny.com>
2021-08-04 17:18:30 +0200

    feat: update OCLUtils to 1.5.0

--- eln/libs/OCLUtils.js
+++ eln/libs/OCLUtils.js
@@ -1,4 +1,4 @@
-import * as OCLUtils from '../../../../../../../../lib/openchemlib-utils/1.4.0/openchemlib-utils';
+import * as OCLUtils from '../../../../../../../../lib/openchemlib-utils/1.5.0/openchemlib-utils';
 import OCL from 'openchemlib/openchemlib-core';
 
 export { OCLUtils, OCL };
