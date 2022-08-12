5c96f43d9abc655a7a2791f72129ffcf3c4db51c
Luc Patiny <luc@patiny.com>
2022-08-12 11:44:56 +0200

    feat: update eln-plugin to 0.31.0

--- eln/libs/elnPlugin.js
+++ eln/libs/elnPlugin.js
@@ -1,1 +1,2 @@
-export { default } from '../../../../../../../../lib/eln-plugin/0.30.0/eln-plugin';
+export { default } from '../../../../../../../../lib/eln-plugin/0.31.0/eln-plugin';
+//export { default } from 'http://localhost:9898/cheminfo/eln-plugin/dist/eln-plugin.js';
