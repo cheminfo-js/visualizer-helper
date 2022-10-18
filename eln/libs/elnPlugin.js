e80a0af7758c72c333e660373bc107fd7d418470
Luc Patiny <luc@patiny.com>
2022-10-18 03:39:37 +0200

    fix: update eln-plugin to 0.32.1

--- eln/libs/elnPlugin.js
+++ eln/libs/elnPlugin.js
@@ -1,2 +1,2 @@
-export { default } from '../../../../../../../../lib/eln-plugin/0.32.0/eln-plugin';
+export { default } from '../../../../../../../../lib/eln-plugin/0.32.1/eln-plugin';
 //export { default } from 'http://localhost:9898/cheminfo/eln-plugin/dist/eln-plugin.js';
