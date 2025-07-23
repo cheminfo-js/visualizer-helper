b86d4ae970f1d6f8d39827e523341d6d85dd8156
Luc Patiny <luc@patiny.com>
2025-07-23 10:58:35 +0200

    fix: do not ensureHighlight if not iterable

--- util/ensureHighlight.js
+++ util/ensureHighlight.js
@@ -1,8 +1,10 @@
 import API from 'src/util/api';
 
 function ensureHighlight(variableName) {
-  let array = API.getData(variableName);
+  const array = API.getData(variableName);
   let changed = false;
+
+  if (!array || typeof array[Symbol.iterator] !== 'function') return changed;
   for (let item of array) {
     if (!item._highlight) {
       Object.defineProperty(item, '_highlight', {
