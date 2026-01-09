74b0577d2e1fe4d5a0e6fd15e619318f630a3d9a
Daniel Kostro <kostro.d@gmail.com>
2026-01-09 16:54:20 +0100

    test: don't transform files when running tests

--- test-build.babel.mjs
+++ test-build.babel.mjs
@@ -1,6 +1,5 @@
 export default {
   ignore: ['node_modules', 'build'],
-  presets: ['@babel/preset-env'],
   plugins: [
     '@babel/plugin-transform-destructuring',
     '@zakodium/babel-plugin-transform-modules-amd',
